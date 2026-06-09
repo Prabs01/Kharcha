edce5a12862fbe94b19f3d27b2195b184194e9cc
Prabesh Bastola <prabeshbastola382@gmail.com>
2026-06-09 23:28:57 +0545

    fix: update import statement for app.models in migrations env.py

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.config import settings
-import app.models as models
+import app.models
 from app.models import SQLModel
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
